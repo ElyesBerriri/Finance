62a411d3f6054ed0b10634951fd3a3a650049c2c
ElyesBerriri <ilyesberriri@gmail.com>
2022-04-26 08:41:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/finance/EpargneMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.finance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EpargneMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_epargne_main);
+    }
+}
